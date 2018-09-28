cd6ea2d4fa5731b7069d85ac9d9fffb03da3b1b6
rishabh-malhotra <rishabhmalhotra110796@gmail.com>
2018-09-28 15:26:30 +0530

    used typescript class syntax

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>{{count}}</h2>
+    <h3>{{halfCount}}</h3>
+    <button @click="incr()">INCREMENT</button>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+@Component
+export default class About extends Vue {
+  count = 0
+  incr() {
+    this.count++
+  }
+
+  get halfCount(){
+    return this.count/2;
+  }
+}
+</script>
